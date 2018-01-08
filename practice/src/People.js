3f6477d06926cd4cf7dc7eecba59b4122a486865
linh309 <linh309@gmail.com>
2018-01-08 23:09:42 +0700

    commit code

--- practice/src/People.js
+++ practice/src/People.js
@@ -15,3 +15,4 @@ class Student extends People{
 }
 
 export {People, Student};
+//export default People;
--- practice/src/index.js
+++ practice/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as lib from './People.js'
+//import People from './People.js';
+import * as lib from './People.js';
 
 //sdasdasdasd asd aa
 var p = new lib.People();
+//var s = new Student();
+
+//console.log(s.getClass());
 console.log(p.getName());
