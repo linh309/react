93cc1a7ffdd8acc33d458c8c4a39810963cf8e0b
lnguyen1@amaris.com <lnguyen1@amaris.com>
2018-01-09 13:54:37 +0700

    learning class

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import People from './People.js';
-import * as lib from './People.js';
 
-//sdasdasdasd asd aa
-var p = new lib.People();
-//var s = new Student();
+//learning class more
+class Student{
+    constructor(info){
+        this.name = info.name;
+        this.course = info.course;
+    }
+    getCourse(){
+        return this.course;
+    }
+    showName(){
+        console.log(`Student is: ${this.name}`);
+    }
+    static getFull(){
+        console.log("Full info");
+    }
+};
 
-//console.log(s.getClass());
-console.log(p.getName());
+let People =  function () {
+    function People(info){
+        this.name = info.name;
+        this.gender = info.gender;
+    }
+    
+    Object.defineProperty(People.prototype,'showName', {
+        key: 'showName',
+        value: function (){
+            console.log(`Name is: ${this.name}`);
+        }
+    })
+
+    Object.defineProperty(People.prototype,'getGender', {
+        key: 'getGender',
+        value: function (){
+            return this.gender;
+        }
+    })
+
+    Object.defineProperty(People,'getFull',{
+        value: 'getFull',
+        value: ()=>{
+            console.log('just simulate static method');
+        }
+    });
+
+    return People;
+}();
+
+//let s = new Student({name: 'Linh', course: 'JS'});
+//s.showName();
+
+
+// let p = new People({name: "ABC", gender: "Male"});
+// var gender = p.getGender();
+// console.log(`Gender is: ${gender}`);
+People.getFull();
+Student.getFull();
+//p.showName();
+//console.log(`Name is: ${p.name}`);
