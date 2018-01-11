9b0fc8149ded6d1e69d9958b69abd4d32af15801
linh309 <linh309@gmail.com>
2018-01-11 21:21:44 +0700

    start learning reactjs

--- practice/src/index.js
+++ practice/src/index.js
@@ -2,11 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import People from './People.js';
-import * as lib from './People.js';
+//import * as lib from './People.js';
 
-//sdasdasdasd asd aa
-var p = new lib.People();
-//var s = new Student();
+// //sdasdasdasd asd aa
+// var p = new lib.People();
+// //var s = new Student();
 
-//console.log(s.getClass());
-console.log(p.getName());
+// //console.log(s.getClass());
+// console.log(p.getName());
+
+
+// function asyncFunc() {
+//     const blank = [];
+//     setTimeout(() => blank.push('DONE'), 5000);
+//     return blank;
+// }
+// const blank = asyncFunc();
+// console.log(blank);
+
+//const wait = time => new Promise((resolve) => setTimeout(resolve, time));
+
+// const wait = time => new Promise((resolve)=>setTimeout(resolve, time));
+
+// const wait = time => new Promise((resolve) => setTimeout(resolve, time));
+// wait(3000)
+//     .then(  ()=>{ console.log("GOOD") }, 
+//             ()=>{ console.log("NOT GOOD") });
+
+// const waitFn = function (time) {
+//     return new Promise(function(resolve){
+//         setTimeout(resolve({time: time, current: new Date()}), time);
+//     });
+// }
+
+// waitFn(2000).then(function(data){    
+//     console.log(`Time is: ${data.time}; Current: ${data.current}}`);
+// })
+
+
+// const wait = time => new Promise((resolve) => setTimeout(resolve, time));
+
+// wait(3000).then(() => console.log('Hello!')); // 'Hello!'
