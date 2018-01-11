e3c7e71b560265454046a62727eecba7278075d6
linh309 <linh309@gmail.com>
2018-01-11 23:14:50 +0700

    update code

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,45 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import People from './People.js';
-//import * as lib from './People.js';
 
-// //sdasdasdasd asd aa
-// var p = new lib.People();
-// //var s = new Student();
+let now = new Date();
+const DateEx = <p>Now is {now.toLocaleDateString()}</p>;
+const dtEx = DateEx;
 
-// //console.log(s.getClass());
-// console.log(p.getName());
+const exDt = now;
 
+function Welcome(props){
+    if (props.isLogin){
+        let now = new Date();
+        let value = '<></a>dsadas><">"';
+        //return <p>You logged at {now.toLocaleDateString()}</p>;
+        return <p>{value}</p>;
+    }
+    else {
+        return <p>please login first</p>;
+    }
+}
 
-// function asyncFunc() {
-//     const blank = [];
-//     setTimeout(() => blank.push('DONE'), 5000);
-//     return blank;
-// }
-// const blank = asyncFunc();
-// console.log(blank);
-
-//const wait = time => new Promise((resolve) => setTimeout(resolve, time));
-
-// const wait = time => new Promise((resolve)=>setTimeout(resolve, time));
-
-// const wait = time => new Promise((resolve) => setTimeout(resolve, time));
-// wait(3000)
-//     .then(  ()=>{ console.log("GOOD") }, 
-//             ()=>{ console.log("NOT GOOD") });
-
-// const waitFn = function (time) {
-//     return new Promise(function(resolve){
-//         setTimeout(resolve({time: time, current: new Date()}), time);
-//     });
-// }
-
-// waitFn(2000).then(function(data){    
-//     console.log(`Time is: ${data.time}; Current: ${data.current}}`);
-// })
+const welcome = Welcome(false);
+debugger;
+ReactDOM.render(<Welcome isLogin={true} />, document.getElementById("root"));
 
 
-// const wait = time => new Promise((resolve) => setTimeout(resolve, time));
-
-// wait(3000).then(() => console.log('Hello!')); // 'Hello!'
+// var tick = setInterval(()=>{
+//     let now = new Date();
+//     ReactDOM.render(<p>Now is {now.toLocaleTimeString()}</p>, document.getElementById("root"));    
+// },1000);
