08aff6497afe7e8b2bf8fabcb9629bdf144de68c
linh309 <linh309@gmail.com>
2018-01-19 23:31:20 +0700

    learning reducer

---
+++ practice/src/reducers.js
@@ -0,0 +1,56 @@
+import {
+    VisibilityFilters, 
+    SET_VISIBILITY_FILTER,
+    ADD_TODO,
+    TOGGLE_TODO
+} from './actions'
+
+const initialState = {
+    visibilityFilter: VisibilityFilters.SHOW_ALL,
+    todos: []
+};
+
+function todos(state=[], action){
+    switch(action.type) {
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        case TOGGLE_TODO:
+            return state.map((todo, index) => {
+                if (index === action.index) {
+                    return Object.assign({}, todo, {
+                        completed: !todo.completed
+                    })
+                }
+                return todo;
+            })
+        default:
+            return state;
+    }
+}
+
+function todoApp(state = initialState, action){
+    if (state === 'undefined'){
+        return initialState;
+    }
+
+    switch(action.type) {
+        case SET_VISIBILITY_FILTER:
+            return Object.assign({}, state,{visibilityFilter:action.filter });
+        case ADD_TODO:
+            return Object.assign({},state,
+                {todos: todos(state.todos, action)});
+        case TOGGLE_TODO:
+            return Object.assign({},state,
+                {todos: todos(state.todos, action)});
+        default: 
+            return state;
+    }
+    
+    return state;
+}
